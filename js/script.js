42bb3dff516de80bb9940726a8fac7b5942b3286
Akhil Balakrishnan <akhilbkrishnan2000@gmail.com>
2022-02-02 12:53:13 +0530

    Display area added, functions for clearAll() del() were defined.

--- js/script.js
+++ js/script.js
@@ -6,3 +6,49 @@ checkbox.addEventListener('change', function () {
     else
         document.documentElement.setAttribute('data-theme', 'light');
 });
+
+let upperData = document.getElementById('upper_value');
+let lowerData = "";
+let data = document.getElementById('lower_value');
+
+function clearAll() {
+    upperData.innerHTML = "";
+    data.innerHTML = "";
+}
+
+function del() {
+    data.innerHTML = data.innerHTML.slice(0, -1)
+}
+
+// function display(_num) {
+//     lowerData += _num;
+//     switch (_num) {
+//         case '+':
+//             upperData.innerHTML = lowerData;
+//             lowerData = "";
+//         case '-':
+//             upperData.innerHTML = lowerData;
+//             lowerData = "";
+//         case '*':
+//             upperData.innerHTML = lowerData;
+//             lowerData = "";
+//         case '/':
+//             upperData.innerHTML = lowerData;
+//             lowerData = "";
+//         case '%':
+//             upperData.innerHTML = lowerData;
+//             lowerData = "";
+//         default:
+//             data.innerHTML = lowerData;
+//     }
+// }
+
+// function calc() {
+//     upperData.innerHTML += data.innerHTML;
+//     try {
+//         data.innerHTML = eval(upperData.innerHTML);
+//     }
+//     catch (err) {
+//         alert('Invalid');
+//     }
+// }
