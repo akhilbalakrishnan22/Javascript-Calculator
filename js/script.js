e9762c208a147498e708a685323bb77517802214
Akhil Balakrishnan <akhilbkrishnan2000@gmail.com>
2022-02-03 17:53:24 +0530

    Designed neumorphic calculator with dark and light theme.Display area has two portions upper value and lower value currently lower value is working, upper value not going to work now.Comments added to the files.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
+/** 
+ * Selecting the input checkbox 
+ */
 let checkbox = document.querySelector('input[name="theme"]');
 
+/**
+ * Through the addEventListener method changing the data-theme of html tag from light to dark.
+ * Here, It listens for the specified event (ie, is "change") and when it occurs it invokes the
+ * anonymous function which checks for whether checkbox is checked or not, If it checked then
+ * changing the data-theme attribute of html tag to dark.
+ * <html lang="en" data-theme="dark">....</html>
+ * and if its unchecked it will change the data-theme attribute to light
+ * <html lang="en" data-theme="light">....</html>
+ */
 checkbox.addEventListener('change', function () {
     if (this.checked)
         document.documentElement.setAttribute('data-theme', 'dark');
@@ -7,48 +19,50 @@ checkbox.addEventListener('change', function () {
         document.documentElement.setAttribute('data-theme', 'light');
 });
 
-let upperData = document.getElementById('upper_value');
-let lowerData = "";
-let data = document.getElementById('lower_value');
 
+/**
+ * currently here the "upperDat, lowerData" are not using. It uses when the expression nee to be
+ * moved to the upper value.And the result will be displayed in the lower value.But now both the
+ * expression and result are showing in the lower value.
+ */
+
+// let upperData = document.getElementById("upper_value");
+// let lowerData = "";
+let data = document.getElementById("lower_value");
+
+
+/** 
+ * Function to clear both upper value and lower value 
+ */
 function clearAll() {
-    upperData.innerHTML = "";
-    data.innerHTML = "";
+    // upperData.innerText = "";
+    data.value = "";
 }
 
+/** 
+ * Function to delete data on the lower value one by one.It slices the data. 
+ */
 function del() {
-    data.innerHTML = data.innerHTML.slice(0, -1)
+    data.value = data.value.slice(0, -1)
 }
 
-// function display(_num) {
-//     lowerData += _num;
-//     switch (_num) {
-//         case '+':
-//             upperData.innerHTML = lowerData;
-//             lowerData = "";
-//         case '-':
-//             upperData.innerHTML = lowerData;
-//             lowerData = "";
-//         case '*':
-//             upperData.innerHTML = lowerData;
-//             lowerData = "";
-//         case '/':
-//             upperData.innerHTML = lowerData;
-//             lowerData = "";
-//         case '%':
-//             upperData.innerHTML = lowerData;
-//             lowerData = "";
-//         default:
-//             data.innerHTML = lowerData;
-//     }
-// }
+/** 
+ * Function to display the value which is clicked by the user 
+*/
+function display(_val) {
+    data.value += _val;
+}
 
-// function calc() {
-//     upperData.innerHTML += data.innerHTML;
-//     try {
-//         data.innerHTML = eval(upperData.innerHTML);
-//     }
-//     catch (err) {
-//         alert('Invalid');
-//     }
-// }
+/** 
+ * Function to evaluate the expression that the user has been entered.The evaluation is carried out
+ * inside the try block and if the expression is an invalid it alerts the user the entered expression
+ * cannot be evaluated.
+ */
+function calc() {
+    try {
+        data.value = eval(data.value);
+    }
+    catch (err) {
+        alert("Invalid");
+    }
+}
